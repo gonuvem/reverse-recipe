af0dd329dda681fa8cb17780f7dde75447ec1611
jeovazero <contato@jeova.ninja>
2021-05-15 01:53:37 -0300

    feat: initial commit

---
+++ assets/readme.md
@@ -0,0 +1,11 @@
+## Assets
+
+### Logo
+
+![](./reverse-recipe-logo.svg)
+
+### Pattern
+
+![](./lunch-pattern.jpg)
+
+<a href="https://www.vecteezy.com/free-vector/food-pattern">Food Pattern Vectors by Vecteezy</a>
---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import logo from '@assets/reverse-recipe-logo.svg'
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />{" "}
+        <p>Reverse Recipe!</p>
+      </header>
+      <footer>
+        <a href='https://www.vecteezy.com/free-vector/food-pattern'>
+          Food Pattern Vectors by Vecteezy
+        </a>
+      </footer>
+    </div>
+  )
+}
+
+export default App
